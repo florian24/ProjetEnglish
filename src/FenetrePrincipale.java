d7a206ce296769e3816b9ac5fab27d3b7fed2b12
florian24 <florian.chevallier1@yahoo.fr>
2015-11-10 19:56:59 +0100

    FenetrePrincipale
    Creation des labels pass et logiin

--- src/FenetrePrincipale.java
+++ src/FenetrePrincipale.java
@@ -1,4 +1,6 @@
 
+import java.awt.Font;
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -8,18 +10,44 @@ public class FenetrePrincipale {
 	public static void main(String[] args) {
 		// TODO On instancie un premiere fenetre
 		JFrame FenetreConnexion = new JFrame();
+		FenetreConnexion.setBounds(100, 100, GblVar.HauteurFenetre,GblVar.LargeurFenetre);
+		FenetreConnexion.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		FenetreConnexion.getContentPane().setLayout(null);
+		
 		//TODO on donne les attributs necessaire a la fenetre 
-		FenetreConnexion.setTitle("Easy Learn Enlish"); 
-		FenetreConnexion.setSize(800,600);
+		FenetreConnexion.setTitle("Easy Learn Enlish");
 		//TODO on Instancie tous les champs presents sur cete fenetre boutons etc.
 		JButton BoutonConnexion = new JButton();
 		JLabel Label_Pseudo = new JLabel();
 		JLabel Label_Password = new JLabel();
 		JLabel Label_TitreFenConnexion = new JLabel();
+		
+		//TODO on defini les polices de titre.
+		Font PoliceTitre = new Font("Arial",Font.BOLD,24);
+		Label_TitreFenConnexion.setFont(PoliceTitre);
+
+		
+		
 		//TODO On definie le texte et la postion des labels.
 		Label_Pseudo.setText("Pseudo :");
+		Label_Password.setText("Mot de passe :");
+		Label_TitreFenConnexion.setText("Easy learn English");
+		
+		//On place les labels
+		Label_Pseudo.setBounds(300,250,100,20);
+		Label_Password.setBounds(300,280,100,20);
+		Label_TitreFenConnexion.setBounds(300,50,250,100);
+		
+		//setBounds(Position x,position y,Largeur label ,Hauteur label)
+		
+		
+		
 		FenetreConnexion.add(Label_Pseudo);
-		// TODO On affiche la fenetre principale
+		FenetreConnexion.add(Label_Password);
+		FenetreConnexion.add(Label_TitreFenConnexion);
+		
+		
+		FenetreConnexion.getContentPane().add(Label_Pseudo);
 		FenetreConnexion.setVisible(true);
 		
 
---
+++ src/GblVar.java
@@ -0,0 +1,9 @@
+
+public class GblVar {
+
+
+	static int LargeurFenetre = 600;
+	static int HauteurFenetre = 800;
+	
+	
+}
